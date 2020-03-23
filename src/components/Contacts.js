fa552c74fa1d208f0eaa84d6f87ce17695a67d29
EddieOrozco <orozcoe230@gmail.com>
2020-03-23 15:16:21 -0500

    Destructured compoents even more (not needed for smaller projects)

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,42 +2,34 @@ import React, { Component } from "react";
 import Contact from "./Contact";
 
 class Contacts extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: "Eddie O",
-          email: "eddieo23@gmail.com",
-          phone: "777-777-7777"
-        },
-        {
-          id: 2,
-          name: "Josh P",
-          email: "josh13@gmail.com",
-          phone: "555-555-5555"
-        },
-        {
-          id: 3,
-          name: "Rascal R",
-          email: "rascal7@gmail.com",
-          phone: "222-222-2222"
-        }
-      ]
-    };
-  }
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "Eddie O",
+        email: "eddieo23@gmail.com",
+        phone: "777-777-7777"
+      },
+      {
+        id: 2,
+        name: "Josh P",
+        email: "josh13@gmail.com",
+        phone: "555-555-5555"
+      },
+      {
+        id: 3,
+        name: "Rascal R",
+        email: "rascal7@gmail.com",
+        phone: "222-222-2222"
+      }
+    ]
+  };
   render() {
     const { contacts } = this.state;
     return (
       <div>
         {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            name={contact.name}
-            email={contact.email}
-            phone={contact.phone}
-          />
+          <Contact key={contact.id} contact={contact} />
         ))}
       </div>
     );
