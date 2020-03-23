ead527954a221245b704fae2a27d1916b446c80f
EddieOrozco <orozcoe230@gmail.com>
2020-03-23 13:17:56 -0500

    Creating simple class / functional components

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Eddie O</h4>
+        <ul>
+          <li>Email: eddieo23@gmail.com</li>
+          <li>Phone: 777-777-7777</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Contact;
